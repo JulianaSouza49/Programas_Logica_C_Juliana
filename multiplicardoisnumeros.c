343121e1a0a4ad013af79d37215e1064795c9b2b
JulianaSouza49 <lj.m.msouza@gmail.com>
2025-12-02 16:42:58 -0300

    Add files via upload

---
+++ multiplicardoisnumeros.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(){
+float numero01;
+float numero02;
+float multiplicar;
+printf("Digite o 1° numero: ");
+scanf("%f",&numero01);
+printf("Digite o 2° numero: ");
+scanf("%f",&numero02);
+multiplicar = numero01 * numero02;
+printf("a multiplicacao eh: %f",multiplicar);
+return 0;
+}
